57794dcdb4b6139f71f301e3768843c4f52c13d0
AndroidHQ254 <d.murimi14@gmail.com>
2024-06-01 23:57:29 +0300

    toco: Copy NFC firmware to the correct location
    Signed-off-by: Dennis Murimi <d.murimi14@gmail.com>

--- toco-vendor.mk
+++ toco-vendor.mk
@@ -158,7 +158,6 @@ PRODUCT_PACKAGES += \
     libhdmiedid \
     libhfp \
     libqtigef \
-    libsn100u_fw \
     libsndmonitor \
     libspkrprot \
     libxlog \
@@ -291,6 +290,7 @@ PRODUCT_PACKAGES += \
     libsdk_sr \
     libsensorcal \
     libsensorslog \
+    libsn100u_fw \
     libsns_device_mode_stub \
     libsns_fastRPC_util \
     libsns_low_lat_stream_stub \
