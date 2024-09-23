84901dfe635bd0646f12f3c8893983ce261c03ae
AndroidHQ254 <d.murimi14@gmail.com>
2024-09-23 22:46:51 +0000

    toco: Drop partition dependent module name for `libmpbase.so`
    Signed-off-by: Dennis Murimi <d.murimi14@gmail.com>

--- toco-vendor.mk
+++ toco-vendor.mk
@@ -275,7 +275,7 @@ PRODUCT_PACKAGES += \
     libmialgo_utils \
     libmialgoengine \
     libmibokeh_712 \
-    libmpbase_vendor \
+    libmpbase \
     libnotifyaudiohal \
     liboemcrypto \
     libremosaic_daemon \
